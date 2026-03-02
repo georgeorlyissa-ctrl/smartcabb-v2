a19b36e62324ffe759bb863dbf3d1d6034f39340
georgeorlyissa-ctrl <georgeorlyissa@gmail.com>
2026-03-02 07:40:20 +0100

    Update BUILD_VERSION.ts

--- BUILD_VERSION.ts
+++ BUILD_VERSION.ts
@@ -3,6 +3,6 @@
  * Auto-généré à chaque build
  */
 
-export const BUILD_VERSION = '518.1.2';
+export const BUILD_VERSION = '518.1.3';
 export const BUILD_TIMESTAMP = new Date().toISOString();
 export const BUILD_ENV = 'production';
